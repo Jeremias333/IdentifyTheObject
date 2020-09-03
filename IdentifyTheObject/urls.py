9c89958c4c8abe00399b3dc81328a7459f614ed6
jeremias <jeremiasprogrammer@gmail.com>
2020-09-03 20:34:15 -0300

    env(setting): arquivos do app main.

--- IdentifyTheObject/urls.py
+++ IdentifyTheObject/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import IdentifyTheObject.main.views as view
 
 # Para submissão de imagens
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", view.index)
 ]
 
 # Para submissão de imagens
